d2ef3ab91308c8024219905df44a122d2c65edb9
Ola-22 <olanazly@gmail.com>
2021-07-11 11:37:12 +0300

    Add Line from chartjs-2 and hooks

--- src/Components/LineGraph/index.js
+++ src/Components/LineGraph/index.js
@@ -1,7 +1,94 @@
 import "./style.css";
 import { Line } from "react-chartjs-2";
+import { useEffect, useState } from "react";
 
+// const data = {
+//   datasets: [
+//     {
+//       type: "line",
+//       backgroundColor: "black",
+//       borderColor: "#5AC53B",
+//       borderWidth: 2,
+//       pointBorderColor: "rgba(0, 0, 0, 0)",
+//       pointBackgroundColor: "rgba(0, 0, 0, 0)",
+//       pointHoverBackgroundColor: "#5AC53B",
+//       pointHoverBorderColor: "#000000",
+//       pointHoverBorderWidth: 4,
+//       pointHoverRadius: 6,
+//       // data: data,
+//     },
+//   ],
+// };
+
+const options = {
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: false,
+      position: "top",
+    },
+    interaction: {
+      mode: "index",
+      intersect: false,
+    },
+  },
+  scales: {
+    // x: [
+    //   {
+    //     type: "time",
+    //     time: {
+    //       format: "MM/DD/YY",
+    //       tooltipFormat: "ll",
+    //     },
+    //     ticks: {
+    //       display: false,
+    //     },
+    //   },
+    // ],
+    // x: {
+    //   display: true,
+    //   title: {
+    //     display: true,
+    //     text: "Month",
+    //   },
+    // },
+    xAxes: [
+      {
+        type: "time",
+        time: {
+          displayFormats: {
+            hour: "MMM DD",
+          },
+          tooltipFormat: "MMM D",
+        },
+      },
+    ],
+    y: {
+      display: false,
+    },
+  },
+};
 export default function LineGraph() {
+  // const [data, setData] = useState({});
+  const [graphData, setGraphData] = useState([]);
+
+  const createMockData = () => {
+    let data = [];
+    let value = 50;
+    for (var i = 0; i < 366; i++) {
+      let date = new Date();
+      date.setHours(0, 0, 0, 0);
+      date.setDate(i);
+      value += Math.round((Math.random() < 0.5 ? 1 : 0) * Math.random() * 10);
+      data.push({ x: date, y: value });
+    }
+    setGraphData(data);
+  };
+
+  useEffect(() => {
+    createMockData();
+  }, []);
+
   const data = [
     {
       x: 10,
@@ -10,6 +97,10 @@ export default function LineGraph() {
     {
       x: 15,
       y: 10,
+    },
+    {
+      x: 12,
+      y: 4,
     },
   ];
   return (
@@ -28,10 +119,11 @@ export default function LineGraph() {
               pointHoverBorderColor: "#000000",
               pointHoverBorderWidth: 4,
               pointHoverRadius: 6,
-              data: data,
+              data: graphData,
             },
           ],
         }}
+        options={options}
       />
     </div>
   );

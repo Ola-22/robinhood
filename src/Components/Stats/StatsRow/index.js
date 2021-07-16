696761c3501f9822d6ea76fd7dddcdd8310b1542
Ola-22 <olanazly@gmail.com>
2021-07-16 15:41:03 +0300

    get data from database

--- src/Components/Stats/StatsRow/index.js
+++ src/Components/Stats/StatsRow/index.js
@@ -1,10 +1,33 @@
+import { db } from "../../../firebase";
 import "./style.css";
 
 export default function StatsRow(props) {
   const percentage = ((props.price - props.openPrice) / props.openPrice) * 100;
-  const getModal = () => {};
+  const buyStock = () => {
+    db.collection("myStocks")
+      .where("ticker", "==", props.name)
+      .get()
+      .then((querySnapshot) => {
+        console.log("query", querySnapshot);
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            db.collection("myStocks")
+              .doc(doc.id)
+              .update({
+                shares: (doc.data().shares += 1),
+              });
+          });
+        } else {
+          db.collection("myStocks").add({
+            ticker: props.name,
+            shares: 1,
+          });
+        }
+      });
+  };
   return (
-    <div className="row" onClick={getModal}>
+    <div className="row" onClick={buyStock}>
       <div className="row__intro">
         <h1>{props?.name}</h1>
         <p>{props.volume && props.volume + " shares"}</p>
@@ -14,7 +37,6 @@ export default function StatsRow(props) {
       </div>
       <div className="row__numbers">
         <p className="row__price">{props.price}</p>
-        {/* <p className="row__percentage"> 200%</p> */}
         <p className="row__percentage"> +{Number(percentage).toFixed(2)}%</p>
       </div>
     </div>
